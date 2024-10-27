363da01c4f01f650701dfacfe010f5049cded134
Fairebol <firuel22@gmail.com>
2024-10-27 17:54:32 -0300

    fix test middleware on '/'

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Middleware\AuthenticateUser;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('index')->middleware(AuthenticateUser::class);
+Route::get('/', [HomeController::class, 'index'])->name('index');
 Route::post('/', [HomeController::class, 'index']);
 
 Route::get('/login', [HomeController::class, 'login'])->name('login');
